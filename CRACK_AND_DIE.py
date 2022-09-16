b9615f90cf1aadd862f93fd78642c2b17e261079
Andy Phu <Andy.jiapao.phu@gmail.com>
2022-09-15 20:13:04 -0500

    jvhj

--- CRACK_AND_DIE.py
+++ CRACK_AND_DIE.py
@@ -1,12 +1,23 @@
+from json import load
 import pygame
 background_colour = (255,255,255)
-(width, height) = (300, 200)
+(width, height) = (1080, 720)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Tutorial 1')
 screen.fill(background_colour)
-pygame.display.flip()
+#pygame.display.flip()
 running = True
+
+
+stickman= pygame.image.load("stickman.png")
+screen.blit(stickman, (100,100))
+
+
 while running:
+  
   for event in pygame.event.get():
+    
+    
+    
     if event.type == pygame.QUIT:
       running = False
